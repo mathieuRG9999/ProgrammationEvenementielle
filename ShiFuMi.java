d37c26a7956b3e6d52b92642dacc35aca922b11e
mathieuRG9999 <137323576+mathieuRG9999@users.noreply.github.com>
2025-01-27 17:07:22 +0100

    Add files via upload
    Fonctionne correctement. Problème uniquement avec le compteur du nombre de manche que je réglerais ensuite

--- ShiFuMi.java
+++ ShiFuMi.java
@@ -1,16 +1,22 @@
 import java.awt.event.*;
 import java.util.Random;
-
+//problème reinitialiser/recommencer
 import javax.swing.*;
 
 
 public class ShiFuMi {
-    static  int scoreJoueur;
+    static  int scoreJoueur1;
+    static int scoreJoueur2;
     static  int scoreIA;
-    static int coup;
+    static int coupJoueur1;
+    static int coupJoueur2;
     static int nbManche;
+    static boolean tourJoueur1 = true;
+
+    boolean aJouerJoueur2;
     public ShiFuMi () {
-        this.coup=-1;
+        this.coupJoueur1=-1;
+        this.coupJoueur2=-1;
         this.scoreIA=0;
         this.scoreIA=0;
         this.nbManche=0;
@@ -44,43 +50,48 @@ public class ShiFuMi {
         return var;
     }
 
-    void incremente(boolean estJoueurGagnant) {
+    void incremente(boolean estJoueurGagnant, boolean estDeuxJoueurs) {
         if (estJoueurGagnant) {
-            scoreJoueur+=1;
+            scoreJoueur1+=1;
         } else {
-            this.scoreIA+=1;
+            if (estDeuxJoueurs) {
+                scoreJoueur2+=1;
+            } else {
+                scoreIA+=1;
+            }
+            
         }
     }
 
-    void scoreDuMoment(int joueur1, int ia) { //normalement ça marche
+    void scoreDuMoment(int joueur1, int ia, boolean estDeuxJoueurs) { //normalement ça marche
         if (joueur1==ia) {
             System.out.println("cas ou les deux coups se valent");
         } else {
             if (joueur1==0) {
                 if (ia==1) {
-                    this.incremente(false);
+                    this.incremente(false, estDeuxJoueurs);
                 } else {
-                    this.incremente(true);
+                    this.incremente(true, estDeuxJoueurs);
                 }
             } else {
                 if (joueur1==1) {
                     if (ia!=0) {
-                        this.incremente(false);
+                        this.incremente(false, estDeuxJoueurs);
                     } else {
-                        this.incremente(true);
+                        this.incremente(true, estDeuxJoueurs);
                     }
                 } else {
                     if (joueur1==2) {
                         if (ia!=1) {
-                            this.incremente((false));
+                            this.incremente(false, estDeuxJoueurs);
                         } else {
-                            this.incremente(true);
+                            this.incremente(true, estDeuxJoueurs);
                         }
                     } else {
                         if (ia==0) {
-                            this.incremente(false);
+                            this.incremente(false, estDeuxJoueurs);
                         } else {
-                            this.incremente(true);
+                            this.incremente(true, estDeuxJoueurs);
                         }
                     }
                 }
@@ -88,85 +99,143 @@ public class ShiFuMi {
         }
     }
 
-    public static void main(String [] args) {
-        var frame=new JFrame("ShiFuMi");
-        frame.setSize(1000,1000);
+    public static void main(String [] args) { //il faudrait recréer un autre truc pour pouvoir continuer à joueur après mais bon
+        ShiFuMi shi=new ShiFuMi();
+        boolean estDeuxJoueurs=true;
 
-        //affichage de texte
-        var texteScore=new JLabel[4];
+
+        //Fenêtre
+        JFrame [] frame;
+        if (estDeuxJoueurs) {
+            frame=new JFrame[2];
+        } else {
+            frame=new JFrame[1];
+        }
+        for (int i = 0; i < frame.length; i++) {
+            frame[i] = new JFrame("ShiFuMi - Joueur " + (i + 1));
+            frame[i].setSize(600, 400);  // Taille réduite pour éviter qu'elles ne soient trop grandes
+            frame[i].setLayout(null);
+            frame[i].setVisible(true);
+            frame[i].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+        
+
+
+        //Affichage txt
+        JLabel [][] texteScore;
+        if (estDeuxJoueurs) {
+           texteScore=new JLabel[2][4];
+        } else {
+            texteScore=new JLabel[1][4];
+        }
+        
         for (int i=0; i<texteScore.length; i++) {
-            texteScore[i]=new JLabel("");
-            texteScore[i].setBounds(400, 400+200*i, 500, 200);
-            frame.add(texteScore[i]);
+            for (int j=0; j<texteScore[i].length; j++) {
+                texteScore[i][j]=new JLabel("");
+                texteScore[i][j].setBounds(400, 400+50*j, 500, 50);
+                frame[i].add(texteScore[i][j]);
+            }
+
         }
 
-        //affichage bouton en plus
+        //affichage bouton en plus, A REVOIR, mon ethique en travail doit absolument revenir sur les rails !
         var re=new JButton[2];
         re[0]=new JButton("REINITIALISER !");
         re[1]=new JButton("RECOMMENCER !");
         for (int i=0; i<re.length; i++) {
-            re[i].setBounds(50,300+200*i,100,100);
-            frame.add(re[i]);
+            re[i].setBounds(50,300+100*i,100,100);
+            frame[i].add(re[i]);
             re[i].addActionListener (e-> {
                 scoreIA=0;
-                scoreJoueur=0;
+                scoreJoueur1=0;
+                scoreJoueur2=0;
                 nbManche=0;
                 for (int j=0; j<texteScore.length; j++) {
-                    texteScore[j].setText("");
+                    for (int u=0; u<texteScore[j].length; u++) {
+                        texteScore[j][u].setText("");
+                    }
                 }
+    
             });
         }
         re[1].setVisible(false);
 
 
 
-        var  button=new JButton[4];
-        ShiFuMi shi=new ShiFuMi();
+        JButton button [][];
+        if (estDeuxJoueurs) {
+            button=new JButton[2][4]; 
+        } else {
+            button=new JButton[1][4];
+        }
+        for (int i=0; i<button.length; i++) {
+            for (int j=0; j<button[i].length; j++) {
+                button[i][j]=new JButton(affichage(j));
+                button[i][j].setBounds(50+100*j,100,95,30);
+                frame[i].add(button[i][j]);
+
+                final int i1=j;
+                button[i][j].addActionListener(e -> {
+                    if (tourJoueur1) {
+                        coupJoueur1 = i1;
+                        tourJoueur1 = false;  // On passe au joueur 2
+                    } else {
+                        coupJoueur2 = i1;
+                        tourJoueur1 = true;  // Retour au joueur 1
+                        shi.scoreDuMoment(coupJoueur1, coupJoueur2, estDeuxJoueurs);  // Évaluation du round
+                        for (int y=0; y<texteScore.length; y++) {
+                            actualiserAffichage(texteScore[y], coupJoueur2, estDeuxJoueurs);
+
+                        }
+                    }
+                });
+            }
+
+        }
+
+
+        if (estDeuxJoueurs) {
+            for (int i=0; i<button.length; i++) {
+                BoutonJeuChoix(button[i], shi, texteScore[i], re, estDeuxJoueurs);
+
+            }
+        } 
+       
+
+    }
+
+
+
+    static void actualiserAffichage(JLabel [] texteScore, int coupIA, boolean estDeuxJoueurs) {
+        System.out.println("score joueur="+scoreJoueur1+"||score IA"+scoreIA);
+        texteScore[0].setText("Le score du joueur est de "+scoreJoueur1);
+        if (estDeuxJoueurs) {
+            texteScore[1].setText("Le score du joueur 2 est de "+scoreIA);
+        } else {
+            texteScore[1].setText("Le score de l'ordinateur est de "+scoreIA);
+        }
+        texteScore[2].setText("L'adversaire a joué "+affichage(coupIA));
+
+        nbManche+=1;
+
+        texteScore[3].setText("On a fait "+nbManche+" manches");
+    }
+
+    static void BoutonJeuChoix(JButton [] button, ShiFuMi shi, JLabel [] texteScore, JButton [] re, boolean estDeuxJoueurs)  {
         for (int i=0; i<button.length; i++) {
             //On initialise nos boutons
+        
             button[i]=new JButton(affichage(i));
-            button[i].setBounds(50+100*i,100,95,30);
-            frame.add(button[i]);
+            
 
             //on les affiche en fonction de la condition
 
-
-            final int i1=i;
-            button[i].addActionListener(e -> {
-                coup=i1;
-                int coupIA=choisirCoupHasard();
-                shi.scoreDuMoment(coup, coupIA);
-                System.out.println("score joueur="+scoreJoueur+"||score IA"+scoreIA);
-                texteScore[0].setText("Le score du joueur est de "+scoreJoueur);
-                texteScore[1].setText("Le score de l'ordinateur est de "+scoreIA);
-                texteScore[2].setText("L'ia a joué "+affichage(coupIA));
-                nbManche+=1;
-
-                texteScore[3].setText("On a fait "+nbManche+" manches");
-
-                if (nbManche>=5) {
-                    //on ne fait pas plus que 5 manches, dans ce cas, on cache les boutons, affiche le gagnat, propose un nouveau match
-                    for (int j=0; j<button.length; j++) {
-                        button[j].setVisible(false);
-                        
-                    }
-                    re[1].setVisible(true);
-                } 
-            });
-
-
-
-
+            
         }
-        frame.setLayout(null);
-
-
-        frame.setVisible(true);
-
-
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+
+
 
 
 }
